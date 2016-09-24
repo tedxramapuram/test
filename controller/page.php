14bb9ca1aba92254c6f9eeb719dbb7adc7c6b8b4
Akshaya Krishnaswamy <akshaya.krishnaswamy@member365.com>
2016-09-24 14:28:44 -0400

    test

---
+++ controller/page.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Akshaya Krishnaswamy
+ * Date: 9/24/2016
+ * Time: 2:05 PM
+ */
+class Page extends CRMPageController
+{
+    public function loginPage(){
+        $username=$this->input->post('username');
+        $password=$this->input->post('password');
+        $response=array();
+
+        if(!($username && $password)){
+            $response['message']="Enter a username and password";}
+
+            elseif(!username){
+            $response['message'] = "Enter a Username";
+        }
+            elseif(!password){
+                $response['message']="Enter a password";
+            }
+           return json_encode($response);
+
+    }
+
+}
---
+++ views/page.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Akshaya Krishnaswamy
+ * Date: 9/24/2016
+ * Time: 12:08 PM
+ */
+?>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <h1>Login</h1>
+<div id="login"
+     <p id="pTag1"></p>
+<label>
+<input id="username" type="text" placeholder="Username" > Username :
+<input id="password" type="text" placeholder="password" >Password :
+    <button type="submit" id="btnSubmit" value="submit">Submit</button>
+</label>
+</div>
+
+<script>
+    $('body').on('click','#btnSubmit',function(e){
+
+        var xdata={};
+        xdata.username = $('#username').val();
+        xdata.password = $('#password').val();
+
+
+        if(!xdata.password){
+            $( "#pTag1" ).append( "Enter a password" );
+        }
+        else
+        {
+            $.ajax({
+                type:"POST",
+                url:'/controller/page',
+                data: xdata,
+                success: function( response ){
+
+                    if( response.success ){
+
+                        $.msgGrowl ({
+                            type: 'success',
+                            text: response.message
+                        });
+
+                    }else {
+                        pTag1.html( response.message );
+                    }
+
+                }
+
+            });
+
+        }
+    });
+</script>
+
